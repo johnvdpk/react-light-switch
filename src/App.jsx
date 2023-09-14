7f57831203a096235ed28dfebe7c64142f1cb854
johnvdpk <johnvdpk@gmail.com>
2023-09-14 10:25:41 +0200

    aan en uitzetten lukt nog niet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const [light, setLight] = useState('');
+    console.log({light})
+
     return (
         <main className="off">
             <section>
-                <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <div className='dot'></div>
+                <button type="button"
+                        className={light  ? 'active' : 'button'}
+                        onClick={()=>setLight('ON')}
+
+                >{light}</button>
+
             </section>
         </main>
     );
